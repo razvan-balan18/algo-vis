097e41957ad0680255e953d908db6d34752630c6
Balan Razvan <razvan10balan10@gmail.com>
2026-06-05 13:47:26 +0300

    basic canvas

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,30 @@ function App() {
   }, []);
 
   const [array, setArray] = useState<number[]>([]);
-  
-  function reset_array(){
+
+  function reset_array() {
     let arr = []
-      for (let i = 0;i <= 15;i++){
-        arr.push(Math.floor(Math.random() * 100) + 1)
-      }
+    for (let i = 0; i <= 40; i++) {
+      arr.push(Math.floor(Math.random() * 100) + 1)
+    }
     setArray(arr)
   }
 
+
+
   return (
-    <div className="flex items-end gap-1 p-4">
-      {array.map((value, index) => (
-        <div key={index} className="w-2 bg-black" style={{ height: `${value}px` }}>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="flex items-end gap-1 p-4">
+        {array.map((value, index) => (
+          <div key={index} className="w-2 bg-black" style={{ height: `${value}px` }}>
+          </div>
+        ))}
+      </div>
+      <button onClick={reset_array} className="px-4 py-2 bg-black text-white rounded hover:bg-gray-800 active:bg-gray-600 transition-colors cursor-pointer ml-4">
+        Generate new array
+      </button>
+    </>
+
   )
 }
 
