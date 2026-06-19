4e8012eb8ff3b6c6f415758f9664e0c29d0d0e70
razvan <razvan10balan10@gmail.com>
2026-06-19 22:11:17 +0300

    individual functionality for press down

---
+++ src/GraphVisualizer/CellBoard.tsx
@@ -0,0 +1,20 @@
+import { CELL_COLORS, type Cell } from "@/types";
+import { memo } from "react";
+
+interface CellBoardProps {
+    cell: Cell
+    onDown: (cell: Cell) => void
+    onEnter: (cell: Cell) => void
+}
+
+export const CellBoard = memo(({ cell, onDown, onEnter }: CellBoardProps) => {
+
+    return (
+        <div
+            onMouseDown={() => onDown(cell)}
+            onMouseEnter={() => onEnter(cell)}
+            className={`h-[22px] w-[22px] border border-slate-200 ${CELL_COLORS[cell.type]}`}
+        />
+    )
+
+});
