ffd50e7980478d8a7dd27984d6eb323cec08006c
razvan <razvan10balan10@gmail.com>
2026-06-20 17:24:25 +0300

    stats panel

--- src/GraphVisualizer/GraphVisualizer.tsx
+++ src/GraphVisualizer/GraphVisualizer.tsx
@@ -1,11 +1,13 @@
 import { Board } from "./Board";
 import { Controls } from "./Controls";
+import { Stats } from "./Stats";
 
 export const GraphVisualizer = () => {
     return (
         <div className="flex flex-col items-center gap-4 p-6">
             <Controls />
             <Board />
+            <Stats />
         </div>
     )
 }
